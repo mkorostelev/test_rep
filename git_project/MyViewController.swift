9c83ec14aeb45af33b624e75af63b06f118e3129
Maksym Korostelov <kostul89@gmail.com>
2017-05-05 18:12:06 +0300

    Added some…

---
+++ git_project/MyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyViewController.swift
+//  git_project
+//
+//  Created by Maksym Korostelov on 5/5/17.
+//  Copyright © 2017 Maksym Korostelov. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
