f57b0d350a37c4b21471f3ea5c8b818b482d28b3
Maksym Korostelov <kostul89@gmail.com>
2017-05-05 17:34:34 +0300

    first

---
+++ git_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_project
+//
+//  Created by Maksym Korostelov on 5/5/17.
+//  Copyright © 2017 Maksym Korostelov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
